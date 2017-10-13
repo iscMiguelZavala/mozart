81f56b49a20d8ce62a657146ab10baf08403173b
Miguel Zavala <isc.miguel.zavala@gmail.com>
2017-10-13 17:55:25 -0500

    permisos de los consumos

--- server.js
+++ server.js
@@ -3,66 +3,76 @@ const path = require("path");
 const fs = require('fs');
 const https = require('https');
 const topojson = require('topojson-client');
+const cors = require('cors');
 
 const app = expres();
 
 const PORT = process.env.PORT || 3000;
 
+//Cors config
+app.use(cors());
+
+app.use(function (req, res, next) {
+    res.setHeader('Acces-Control-Origin', '*');
+    res.setHeader('Acces-Control-Methods', 'GET');
+    next();
+});
+
 // Arrow function ()=>{}
-app.get('/',function(req,res){
+app.get('/', function (req, res) {
     res.send("Bienvenidos a la geo api")
 });
 
 var apiV1 = expres.Router();
 var apiV2 = expres.Router();
 
-apiV1.get('/paises/estados/ciudades', function(req,res){
+apiV1.get('/paises/estados/ciudades', function (req, res) {
     var ciudades = [];
     var worldData = []
     var worldJson = 'https://unpkg.com/world-atlas@1.1.4/world/110m.json';
-    fs.readFile('data/cities.json','UTF-8' ,function (err, data){
+    fs.readFile('data/cities.json', 'UTF-8', function (err, data) {
         //console.log(err,data);
         ciudades = JSON.parse(data).states;
 
-        https.get(worldJson, function(response){
-        
-        var body = '';
+        https.get(worldJson, function (response) {
 
-        response.on('data', function(d){
-            body += d;
-        });
-        response.on('end', function(){
-            try{
-                //worldData = JSON.parse(body);
-                worldData = JSON.parse(body);
-                worldData = topojson.feature(worldData, worldData.objects.countries);
+            var body = '';
 
-                //console.log(JSON.parse(body).d);
-                res.status(200).json({
-                    status: true,
-                    ciudades: ciudades,
-                    worldData: worldData
-                });
+            response.on('data', function (d) {
+                body += d;
+            });
+            response.on('end', function () {
+                try {
+                    //worldData = JSON.parse(body);
+                    worldData = JSON.parse(body);
+                    worldData = topojson.feature(worldData, worldData.objects.countries);
 
-            }catch(err){
-                console.error('Error: ',err)
-            }
-        });
+                    //console.log(JSON.parse(body).d);
+                    res.status(200).json({
+                        status: true,
+                        ciudades: ciudades,
+                        worldData: worldData
+                    });
+
+                } catch (err) {
+                    console.error('Error: ', err)
+                }
+            });
 
             //console.log(response);
         })
 
-        
-    });
-    
-    
 
-   
+    });
+
+
+
+
 
     //res.send('Bienvenido al API 1');
 });
-apiV2.get('/', function(req,res){
-  
+apiV2.get('/', function (req, res) {
+
     var estados = [{
         name: 'Aguascalientes',
         pupulation: 123456
@@ -71,11 +81,11 @@ apiV2.get('/', function(req,res){
         name: 'Guerrero',
         pupulation: 123456
     }
-]
-res.status(200).json({
-    status: true,
-    estados: estados
-});
+    ]
+    res.status(200).json({
+        status: true,
+        estados: estados
+    });
 
     //res.send('Bienvenido al API 2');
 });
@@ -85,7 +95,7 @@ app.use('/api/v2', apiV2);
 
 
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 })
 
